6eded3d8ccb62716da579790e516a6aea2ff73b2
Jarrod Watts <jarrodwatts16@gmail.com>
2022-08-23 14:18:24 +1000

    Add head

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import GithubTriangle from "../components/GithubTriangle";
+import Head from "next/head";
 import "../styles/globals.css";
 
 // This is the chainId your dApp will work on.
@@ -8,6 +9,14 @@ const activeChainId = ChainId.Goerli;
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <title>thirdweb Web3Button</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="thirdweb Example Repository to show how to use the Web3Button UI Component"
+        />
+      </Head>
       <Component {...pageProps} />
       <GithubTriangle />
     </ThirdwebProvider>
