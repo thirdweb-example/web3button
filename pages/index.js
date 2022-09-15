33fb516f17b283f5447fa2c0fe5221da5115435d
Jarrod Watts <jarrodwatts16@gmail.com>
2022-09-16 07:16:31 +1000

    Update to v3

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import { useAddress, Web3Button } from "@thirdweb-dev/react";
+import { Web3Button } from "@thirdweb-dev/react";
 import { useRef, useState } from "react";
 import CodeSnippet from "../components/DemoCodeSnippet";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const address = useAddress();
   const fileInputRef = useRef(null);
   const [file, setFile] = useState();
 
@@ -38,7 +37,7 @@ export default function Home() {
       {file ? (
         <img
           src={URL.createObjectURL(file)}
-          style={{ cursor: "pointer", maxHeight: 250, borderRadius: 8 }}
+          className={styles.imagePreview}
           onClick={() => setFile(undefined)}
         />
       ) : (
@@ -66,8 +65,8 @@ export default function Home() {
         // The contract address
         contractAddress="0x424037abd63d32595bD843791ab015C31c87Cb6d"
         // Access the contract itself, perform any action you want on it:
-        action={(contract) =>
-          contract.nft.mint.to(address, {
+        action={async (contract) =>
+          contract.erc721.mint({
             name: "Hello world!",
             // Image can be a File, or any url that points to a file.
             image: file,
@@ -75,7 +74,7 @@ export default function Home() {
           })
         }
         // Some customization of colors and styling
-        colorMode="light"
+        colorMode="dark"
         accentColor="#F213A4"
         // If the function is successful, we can do something here.
         onSuccess={(result) => console.log(result)}
@@ -97,21 +96,14 @@ export default function Home() {
     // The contract address
     contractAddress="0x424037abd63d32595bD843791ab015C31c87Cb6d"
     // Access the contract itself, perform any action you want on it:
-    action={(contract) =>
-      contract.nft.mint.to(address, {
+    action={async (contract) =>
+      contract.erc721.mint({
         name: "Hello world!",
-        // Image can be a File, or any url that points to a file
+        // Image can be a File, or any url that points to a file.
         image: file,
         description: "Your awesome NFT",
       })
     }
-    // Some customization of colors and styling
-    colorMode="light"
-    accentColor="#F213A4"
-    // If the function is successful, we can do something here.
-    onSuccess={(result) => console.log(result)}
-    // If the function fails, we can do something here.
-    onError={(error) => console.error(error)}
   >
     Mint NFT
   </Web3Button>
